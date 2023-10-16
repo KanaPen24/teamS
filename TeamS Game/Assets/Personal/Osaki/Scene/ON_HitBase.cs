1c88d3fdbb4a287c18a37c1faefac4182d937b1f
noriaki222 <osakinoriaki222@gmail.com>
2023-10-16 10:49:30 +0900

    Create:Hit

---
+++ TeamS Game/Assets/Personal/Osaki/Scene/ON_HitBase.cs
@@ -0,0 +1,74 @@
+/**
+@file   ON_HitBase.cs
+@brief  当たり判定クラス
+@author Norialo Osaki
+@date   2023/10/13
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 当たり判定のタイプ
+public enum HitType
+{
+    NONE = 0,
+    ATTACK,
+    RECEIVE,
+    FIELD,
+
+    MAX_TYPE,
+}
+
+public class ON_HitBase
+{
+    // コンストラクタ
+    public ON_HitBase(Vector2 center, Vector2 size, int ID) : this(center, size, true, HitType.RECEIVE, ID)
+    {
+        m_hitID = 0;
+    }
+
+    public ON_HitBase(Vector2 center, Vector2 size, bool active, HitType type, int ID)
+    {
+        m_center = center;
+        m_size = size;
+        m_active = active;
+        m_type = type;
+        m_objID = ID;
+        m_hitID = 0;
+    }
+
+    // 中心
+    public void SetCenter(Vector2 value) { m_center = value; }
+    public Vector2 GetCenter() { return m_center; }
+
+    // 大きさ
+    public void SetSize(Vector2 value) { m_size = value; }
+    public Vector2 GetSize() { return m_size; }
+
+    // 有効
+    public void SetActive(bool value) { m_active = value; }
+    public bool GetActive() { return m_active; }
+
+    // タイプ
+    public void SetHitType(HitType value) { m_type = value; }
+    public HitType GetHitType() { return m_type; }
+
+    // objID
+    public void SetObjID(int value) { m_objID = value; }
+    public int GetObjID() { return m_objID; }
+
+    // hitID
+    public void SetHitID(int value) { m_hitID = value; }
+    public int GetHitID() { return m_hitID; }
+
+    // 当たり判定の移動
+    public void MoveHit(Vector2 vec) { m_center += vec; }
+
+    private Vector2   m_center; // 当たり判定の中心
+    private Vector2   m_size;   // 当たり判定の大きさ(中心からの距離
+    private bool      m_active; // 当たり判定が有効か(true:有効 false:無効
+    private HitType   m_type;   // 当たり判定のタイプ
+    private int       m_objID;  // 当たり判定のあるオブジェクトID
+    private int       m_hitID;  // 当たり判定の識別ID
+}
