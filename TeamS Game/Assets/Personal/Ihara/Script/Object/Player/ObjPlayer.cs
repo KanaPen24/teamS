4a77ceb48c16685e18dc69dd1af89029d4224a13
IharaShota <shotaihara1106@gmail.com>
2023-10-27 15:54:07 +0900

    プレイヤーの基本移動操作終了(攻撃はまた後で)

--- TeamS Game/Assets/Personal/Ihara/Script/Object/Player/ObjPlayer.cs
+++ TeamS Game/Assets/Personal/Ihara/Script/Object/Player/ObjPlayer.cs
@@ -9,6 +9,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerState
+{
+    Idle,
+    Walk,
+    //Dash,
+    Jump,
+    Drop,
+    //Atk,
+    //Def,
+
+    MaxPlayerState
+}
+
 public class ObjPlayer : ObjBase
 {
     public static bool m_bJumpFlg;
@@ -17,53 +30,73 @@ public class ObjPlayer : ObjBase
     public static bool m_bDefFlg;
     public static bool m_bWalkFlg;
 
-    public void Update()
-    {
-        // --- 入力確認 ---
-        // 移動
-        if (IS_XBoxInput.LStick_H > 0.2f || IS_XBoxInput.LStick_H < -0.2f)
-        {
-            m_vSpeed.x += IS_XBoxInput.LStick_H * m_fAccel;
+    public static ObjPlayer instance;
+    public List<PlayerStrategy> m_PlayerStrategys;
+    public PlayerState m_PlayerState;
 
-            if (m_vSpeed.x > m_vMaxSpeed.x)
-            {
-                m_vSpeed.x = m_vMaxSpeed.x;
-            }
-            if (m_vSpeed.x < -m_vMaxSpeed.x)
-            {
-                m_vSpeed.x = -m_vMaxSpeed.x;
-            }
+    public void Start()
+    {
+        // インスタンス化
+        if (instance == null)
+        {
+            instance = this;
         }
         else
         {
-            m_vSpeed.x *= 0.99f;
-            if (m_vSpeed.x <= 0.01f && m_vSpeed.x >= 0.01f)
-            {
-                m_vSpeed.x = 0f;
-            }
+            Destroy(this.gameObject);
         }
-        // ----------------
+    }
 
-        if (Input.GetKeyDown(IS_XBoxInput.B))
-        {
-            if(GameManager.IsDebug())
-                Debug.Log("HitGenerate");
+    public void Update()
+    {
+        // --- 遷移状態による入力確認 ---
+        m_PlayerStrategys[(int)m_PlayerState].UpdateInput();
 
-            ON_HitManager.instance.GenerateHit(this.gameObject.transform.position + new Vector3(1f, 0f, 0f),
-                GetSetScale / 2f, true, HitType.ATTACK, m_nObjID);
-        }
+        //// 移動
+        //if (IS_XBoxInput.LStick_H > 0.2f || IS_XBoxInput.LStick_H < -0.2f)
+        //{
+        //    m_vSpeed.x += IS_XBoxInput.LStick_H * m_fAccel;
 
-        if (Input.GetKeyDown(IS_XBoxInput.A) || Input.GetKeyDown(KeyCode.A))
-        {
-            if (GameManager.IsDebug())
-                Debug.Log("Jump");
-            m_vSpeed.y = 0.7f;
-            m_Ground.GetSetStand = false;
-        }
+        //    if (m_vSpeed.x > m_vMaxSpeed.x)
+        //    {
+        //        m_vSpeed.x = m_vMaxSpeed.x;
+        //    }
+        //    if (m_vSpeed.x < -m_vMaxSpeed.x)
+        //    {
+        //        m_vSpeed.x = -m_vMaxSpeed.x;
+        //    }
+        //}
+        //else
+        //{
+        //    m_vSpeed.x *= 0.99f;
+        //    if (m_vSpeed.x <= 0.01f && m_vSpeed.x >= 0.01f)
+        //    {
+        //        m_vSpeed.x = 0f;
+        //    }
+        //}
+        //// ----------------
+
+        //if (Input.GetKeyDown(IS_XBoxInput.B))
+        //{
+        //    if(GameManager.IsDebug())
+        //        Debug.Log("HitGenerate");
+
+        //    ON_HitManager.instance.GenerateHit(this.gameObject.transform.position + new Vector3(1f, 0f, 0f),
+        //        GetSetScale / 2f, true, HitType.ATTACK, m_nObjID);
+        //}
+
+        //if (Input.GetKeyDown(IS_XBoxInput.A) || Input.GetKeyDown(KeyCode.A))
+        //{
+        //    if (GameManager.IsDebug())
+        //        Debug.Log("Jump");
+        //    m_vSpeed.y = 0.7f;
+        //    m_Ground.GetSetStand = false;
+        //}
     }
 
     public override void UpdateObj()
     {
+        m_PlayerStrategys[(int)m_PlayerState].UpdatePlayer();
     }
 
     public override void UpdateDebug()
---
+++ TeamS Game/Assets/Personal/Ihara/Script/Object/Player/PlayerIdle.cs
@@ -0,0 +1,43 @@
+/**
+ * @file   PlayerIdle.cs
+ * @brief  Playerの「待ち」クラス
+ * @author IharaShota
+ * @date   2023/10/27
+ * @Update 2023/10/27 作成
+ **/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerIdle : PlayerStrategy
+{
+    public override void UpdateInput()
+    {
+        // 待ち → 落下
+        if (!ObjPlayer.instance.GetSetGround.GetSetStand)
+        {
+            ObjPlayer.instance.m_PlayerState = PlayerState.Drop;
+            return;
+        }
+        // 待ち → 移動
+        if (IS_XBoxInput.LStick_H > 0.2f || IS_XBoxInput.LStick_H < -0.2f)
+        {
+            ObjPlayer.instance.m_PlayerState = PlayerState.Walk;
+            return;
+        }
+        // 待ち → 跳躍
+        if (Input.GetKeyDown(IS_XBoxInput.A))
+        {
+            ObjPlayer.instance.m_PlayerState = PlayerState.Jump;
+            ObjPlayer.instance.GetSetSpeed = new Vector2(ObjPlayer.instance.GetSetSpeed.x, 0.7f);
+            ObjPlayer.instance.GetSetGround.GetSetStand = false;
+            return;
+        }
+    }
+
+    public override void UpdatePlayer()
+    {
+        // 速度は0に一定
+        ObjPlayer.instance.GetSetSpeed = new Vector2(0f, 0f);
+    }
+}
