0a819922cdf72f689e7df8b4debd69801f26792f
Kanase <pengingame2@gmail.com>
2023-11-07 18:37:46 +0900

    Update YK_Time.cs

--- TeamS Game/Assets/Personal/Kanase/Script/UI/YK_Time.cs
+++ TeamS Game/Assets/Personal/Kanase/Script/UI/YK_Time.cs
@@ -1,3 +1,9 @@
+/**
+ * @file   YK_Time.cs
+ * @brief  経過時間標示
+ * @author 吉田叶聖
+ * @date   2023/11/07
+ */
 using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
@@ -6,9 +12,9 @@ public class YK_Time : MonoBehaviour
 {
 
     [SerializeField]
-    private int minute;
+    private int m_nMinute;
     [SerializeField]
-    private float seconds;
+    private float m_fSeconds;
     //　前のUpdateの時の秒数
     private float oldSeconds;
     //　タイマー表示用テキスト
@@ -16,25 +22,27 @@ public class YK_Time : MonoBehaviour
 
     void Start()
     {
-        minute = 0;
-        seconds = 0f;
+        m_nMinute = 0;
+        m_fSeconds = 0f;
         oldSeconds = 0f;
         timerText = GetComponentInChildren<Text>();
+       
     }
 
     void Update()
     {
-        seconds += Time.deltaTime;
-        if (seconds >= 60f)
+        //時間経過
+        m_fSeconds += Time.deltaTime;
+        if (m_fSeconds >= 60f)
         {
-            minute++;
-            seconds = seconds - 60;
+            m_nMinute++;
+            m_fSeconds = m_fSeconds - 60;
         }
         //　値が変わった時だけテキストUIを更新
-        if ((int)seconds != (int)oldSeconds)
+        if ((int)m_fSeconds != (int)oldSeconds)
         {
-            timerText.text = minute.ToString("00") + ":" + ((int)seconds).ToString("00");
+            timerText.text = m_nMinute.ToString("00") + ":" + ((int)m_fSeconds).ToString("00");
         }
-        oldSeconds = seconds;
+        oldSeconds = m_fSeconds;
     }
 }
