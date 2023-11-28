95a61922db9ecfd4a4c16e66ef429adba9aa3031
naitoo0205 <100120605+naitoo0205@users.noreply.github.com>
2023-11-28 17:13:57 +0900

    Hitotumeprefab Update

--- TeamS Game/Assets/Personal/Naitoooooo/Scripts/Fly/FlyKnockBack.cs
+++ TeamS Game/Assets/Personal/Naitoooooo/Scripts/Fly/FlyKnockBack.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class FlyKnockBack : FlyStrategy
 {
+    private FlyEnemy m_Fly;
+    private bool endFlg;
     public override void UpdateState()
     {
+
     }
 
     public override void UpdateStrategy()
