8dd6c2d0f74d5c8072c2699719bb97d8ca1861be
IharaShota <shotaihara1106@gmail.com>
2023-12-14 02:00:37 +0900

    ゲームシーン更新(敵の配置はだれかヘルプ),当たり判定修正

--- TeamS Game/Assets/Personal/Ihara/Script/Object/Player/Animation/PlayerAnim.cs
+++ TeamS Game/Assets/Personal/Ihara/Script/Object/Player/Animation/PlayerAnim.cs
@@ -54,10 +54,17 @@ public class PlayerAnim : MonoBehaviour
 
     private void Update()
     {
-        //if(HitStop.instance.hitStopState == HitStopState.ON)
-        if(ObjPlayer.instance.GetSetHitStopParam.m_bHitStop)
+        if (GameManager.GetSetGameState == GameState.GamePause)
+        {
+            m_animator.SetFloat("Speed", 0.0f);
+            return;
+        }
+        else m_animator.SetFloat("Speed", 1f);
+
+        if (ObjPlayer.instance.GetSetHitStopParam.m_bHitStop)
         {
             m_animator.SetFloat("Speed", 0.1f);
+            return;
         }
         else m_animator.SetFloat("Speed", 1f);
     }
